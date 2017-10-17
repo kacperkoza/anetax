60bd8ab9d1a935ac7d6e754e58d540b069c28f52
Kacper Koza <kacper.koza@allegrogroup.com>
2017-10-17 07:53:36 +0200

    add product layout

--- app/src/main/java/com/nostra/koza/anetax/MainActivity.kt
+++ app/src/main/java/com/nostra/koza/anetax/MainActivity.kt
@@ -2,36 +2,35 @@ package com.nostra.koza.anetax
 
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.design.widget.BottomNavigationView.OnNavigationItemSelectedListener
 import android.support.v7.app.AppCompatActivity
-import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    private var mTextMessage: TextView? = null
-
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+    private val mOnNavigationItemSelectedListener = OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_add -> {
-                mTextMessage!!.setText(R.string.title_add)
+                beginFragmentTransaction()
+                        .replace(R.id.content, AddProductFragment())
+                        .commit()
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_list -> {
-                mTextMessage!!.setText(R.string.title_list)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_export -> {
-                mTextMessage!!.setText(R.string.title_export)
+
                 return@OnNavigationItemSelectedListener true
             }
         }
         false
     }
 
+    private fun beginFragmentTransaction() = supportFragmentManager.beginTransaction()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        mTextMessage = findViewById(R.id.message) as TextView
         val navigation = findViewById(R.id.navigation) as BottomNavigationView
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
